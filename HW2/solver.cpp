1ff1b2155bbe49e7746f714e145ed8deef0c11ce
Jinu <jimjang95@gmail.com>
2019-03-05 09:40:15 +0900

    Sequential solver created

--- HW2/solver.cpp
+++ HW2/solver.cpp
@@ -1,21 +1,41 @@
 #include "solver.h"
+#include <cmath>
 
 
 /*************************** DECLARE YOUR HELPER FUNCTIONS HERE ************************/
+void seq_recursion(
+	unsigned int n,
+	std::vector<std::vector<unsigned int> >& all_solns,
+	std::vector<unsigned int> partialSol,
+	unsigned int curN
+);
 
-
-
+bool check(std::vector<unsigned int>& partialSol);
 
 
 /*************************** solver.h functions ************************/
 
 
-void seq_solver(unsigned int n, std::vector<std::vector<unsigned int> >& all_solns) {
+void seq_solver(
+	unsigned int n,
+	std::vector<std::vector<unsigned int> >& all_solns
+) {
 
 	// TODO: Implement this function
+	if (n < 1) {
+		return;
+	}
 
+	std::vector<unsigned int> v;
+	// for (unsigned int i = 0; i < n; i++) {
+	// 	v.push_back(i);
+	// }
+	// for (std::vector<unsigned int>::const_iterator i = v.begin(); i != v.end(); ++i) {
 
+	// 	std::cout << *i << ' ';
 
+	// }
+	return seq_recursion(n, all_solns, v, 0);
 }
 
 
@@ -103,8 +123,44 @@ void nqueen_worker(	unsigned int n,
 
 /*************************** DEFINE YOUR HELPER FUNCTIONS HERE ************************/
 
+void seq_recursion(unsigned int n,
+					std::vector<std::vector<unsigned int> >& all_solns,
+					std::vector<unsigned int> partialSol,
+					unsigned int curN)
+{
+	if (n == 0) {
+		// no way this gets activated but just in case.
+		std::cout << "Scream and shout! The End Is Coming!";
+		return;
+	}
 
+	if (curN < n) {
+		for (int next = 0; next < (int)n; next++) {
+			partialSol.push_back(next);
+			if (check(partialSol)) {
+				seq_recursion(n, all_solns, partialSol, curN + 1);
+			}
+			partialSol.pop_back();
+		}
+	}
 
+	if (curN == n) {
+		// [END condition]
+		all_solns.push_back(partialSol);
+	}
+}
+
+bool check(std::vector<unsigned int>& partialSol) {
+	int size = partialSol.size();
+	unsigned int cur = partialSol[size - 1];
+	for (int i = 0; i < size - 1; i++) {
+		if (partialSol[i] == cur || std::abs(size - 1 - i) == std::abs((int)partialSol[i] - (int)cur)) {
+			// std::cout << "i:" << i << "|| size:" << size << "|| partialSol[i]" << partialSol[i] << "|| cur:" << cur << std::endl;
+			return false;
+		}
+	}
+	return true;
+}
 
 
 
